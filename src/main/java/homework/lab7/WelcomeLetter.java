cf3060db63cdf8e102f223db74942749c0d9ad02
avomika <jane_muse@mail.ru>
2021-08-20 15:52:50 +0300

    mail sender

---
+++ src/main/java/homework/lab7/WelcomeLetter.java
@@ -0,0 +1,8 @@
+package homework.lab7;
+
+public class WelcomeLetter implements Letter{
+    public void content() {
+        //50 lines of code which send welcome mail
+        System.out.println("Welcome mail was sent");
+    }
+}
