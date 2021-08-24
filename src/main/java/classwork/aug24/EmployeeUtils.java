b98568940f22c9f93aaa62dd2868acf92d43962a
avomika <jane_muse@mail.ru>
2021-08-24 22:02:02 +0300

    delete unnecessary libraries

--- src/main/java/classwork/aug24/EmployeeUtils.java
+++ src/main/java/classwork/aug24/EmployeeUtils.java
@@ -4,7 +4,6 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
-import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 public class EmployeeUtils {
